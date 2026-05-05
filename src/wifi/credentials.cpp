83ef9d49cc7e827d25aab9c2dbc7ba82413affcc
Eric Kinolik <ekinolik@gmail.com>
2026-05-04 20:24:30 -0700

    do not store creds if theyre not new

--- src/wifi/credentials.cpp
+++ src/wifi/credentials.cpp
@@ -5,11 +5,22 @@ Wifi::CredentialData Wifi::Credentials::m_credentials;
 namespace Wifi {
     CredentialData Credentials::get() {
         if (m_credentials.isValid()) {
+            Serial.printf("[CREDENTIALS] Valid credentials for %s found in RAM\n", m_credentials.SSID.c_str());
             return m_credentials;
         }
 
         // No valid credentials in RAM, try storage
         m_credentials = getFromStorage();
+
+        if (m_credentials.isValid()) {
+            Serial.printf("[CREDENTIALS] Valid credentials for %s found in storage\n", m_credentials.SSID.c_str());
+        } else {
+            // No valid credentials in storage, use from config
+            String ssid = AppConfig::WIFI_SSID;
+            String password = AppConfig::WIFI_PASSWORD;
+            m_credentials = CredentialData{ssid, password};
+            Serial.printf("[CREDENTIALS] No valid credentials found in storage. Loaded credentials for %s from config\n", m_credentials.SSID.c_str());
+        }
 
         return m_credentials;
     }
@@ -17,16 +28,13 @@ namespace Wifi {
     CredentialData Credentials::getFromStorage() {
         Preferences prefs;
 
-        String ssid = AppConfig::WIFI_SSID;
-        String password = AppConfig::WIFI_PASSWORD;
+        String ssid;
+        String password;
         if (prefs.begin("wifi", true)) {
             ssid = prefs.getString("ssid", AppConfig::WIFI_SSID);
             password = prefs.getString("password", AppConfig::WIFI_PASSWORD);
-            Serial.println("[CREDENTIALS] Loaded credentials from storage");
         }
         prefs.end();
-
-        Serial.printf("[CREDENTIALS] Loaded credentials. SSID=%s, Password=%s\n", ssid.c_str(), password.c_str());
 
         return CredentialData{ssid, password};
     }
@@ -56,14 +64,23 @@ namespace Wifi {
             return false;
         }
 
+        CredentialData tmp_credentials = Credentials::getFromStorage();
+        bool newSSID = true, newPass = true;
+        if (tmp_credentials.SSID == creds.SSID) {
+            Serial.println("[CREDENTIALS] SSID is the same as stored value, not updating storage");
+            newSSID = false;
+        }
+        if (tmp_credentials.PASSWORD == creds.PASSWORD) {
+            Serial.println("[CREDENTIALS] Password is the same as stored value, not updating storage");
+            newPass = false;
+        }
+
         Preferences prefs;
         prefs.begin("wifi", false); // read-write
-        prefs.putString("ssid", creds.SSID);
-        prefs.putString("password", creds.PASSWORD);
-
-        String tmp_ssid = prefs.getString("ssid", "n/a");
-        String tmp_pass = prefs.getString("password", "n/a");
-        Serial.printf("[CREDENTIALS] Saved credentials. SSID=%s, Password=%s\n", tmp_ssid.c_str(), tmp_pass.c_str());
+        // Only update storage if values are different to avoid unnecessary flash writes
+        if (newSSID) prefs.putString("ssid", creds.SSID);
+        if (newPass) prefs.putString("password", creds.PASSWORD);
+        prefs.end();
 
         m_credentials = creds;
 
